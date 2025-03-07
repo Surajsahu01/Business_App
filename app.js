e72ccaab60938218b2472d230d7686ecc6a97197
surajsahu01 <surajsahu6365@gmail.com>
2025-03-07 23:49:08 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { config } from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import authrouter from "./routes/authRoutes.js";
 
 
 
@@ -8,8 +10,12 @@ export const app = express();
 
 config({path: "./config/config.env"});
 
+app.use(cors())
+
 
 //  Add Mideleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
+
+app.use("/api/v1", authrouter);
