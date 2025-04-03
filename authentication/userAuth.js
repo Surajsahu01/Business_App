5d64190628f15e99d133cd956d9314c078470b3e
surajsahu01 <surajsahu6365@gmail.com>
2025-04-04 02:02:04 +0530

    Add

--- authentication/userAuth.js
+++ authentication/userAuth.js
@@ -34,4 +34,13 @@ const authenticateUser = async (req, res, next) => {
     }
 };
 
-export { generateToken, authenticateUser };
+const authorization = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.userExists.role)) {
+            return res.status(403).json({ message: "Forbidden: You do not have permission to access this resource" });
+        }
+        next();
+    };
+}
+
+export { generateToken, authenticateUser, authorization };
